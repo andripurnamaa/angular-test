e03466ca7e7f01e1475bf9bc1b001f306f1f7792
andripurnama66 <kharismandrifp@gmail.com>
2020-07-18 01:38:43 +0700

    add contact and contact list route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { ContactDetailComponent } from "./contact-detail/contact-detail.component"
+import { ContactComponent } from "./contact/contact.component"
+const routes: Routes = [
+  {path: 'contact/:id' , component: ContactDetailComponent},
+  {path: 'contacts' , component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
